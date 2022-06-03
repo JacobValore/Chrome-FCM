a827012154d5b22894c68e4379e16bc2a135b6e2
JacobValore <jacobvalore@gmail.com>
2022-06-03 11:59:43 -0600

    full project

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,6 @@
-// You can find this ID by going to your Firebase project settings
+// You can find the Sender ID by going to your Firebase project settings
 // and going to the "Cloud Messaging" tab.
-chrome.gcm.register(["704490568068"], (registration_id) => {
+chrome.gcm.register(["<Sender ID>"], (registration_id) => {
 	console.log("Registration ID: ", registration_id);
 	if(chrome.runtime.lastError)
 		console.log("Registration Failed");
@@ -12,16 +12,11 @@ chrome.gcm.onMessage.addListener((message) => {
 	console.log("Message: ", message);
 });
 
-// This will send a message to FCM when the action icon is clicked.
-chrome.action.onClicked.addListener((tab) => {
-	console.log(tab);
-	var message = {type: "GetUserID", extraInfo: 42};
-	chrome.gcm.send(message, (messageID) => {
-		console.log("MessageID: ",messageID);
-	});
-});
-
-//This will execute if it was not possible to send a message to the FCM server.
-chrome.gcm.onSendError((error) => {
-	console.log(error);
-});
+// // This will send a message to FCM when the action icon is clicked.
+// chrome.action.onClicked.addListener((tab) => {
+// 	console.log(tab);
+// 	var message = {type: "GetVariable", extraInfo: 42};
+// 	chrome.gcm.send(message, (messageID) => {
+// 		console.log("MessageID: ",messageID);
+// 	});
+// });
---
+++ node_server/Server.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+// Firebase Setup
+const admin = require("firebase-admin");
+const serviceAccount = require("./fcm-private-key.json");
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount)
+});
+
+// Start Server
+const server_port = process.env.PORT || 8080;
+server.listen(server_port, function(){
+	console.log("listening on port " + server_port);
+});
+app.get('*', function(req, res){
+	res.sendFile(__dirname + '/index.html');
+});
+
+// I got this token by running the chrome extension and checking the console
+const registrationToken = '<RegistrationID>';
+io.on('connection', function(socket){
+	socket.on('send_FCM_message', function(data){
+		var message = {token: registrationToken, data: data};
+		admin.messaging().send(message).then((response) => {
+			console.log('Successfully sent message:', response);
+		}).catch((error) => {
+			console.log('Error sending message:', error);
+		});
+	});
+});
